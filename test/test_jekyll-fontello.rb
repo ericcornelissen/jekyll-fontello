6e866f8a0fc23c99aab8fd5c7d92336748330a9c
Eric Cornelissen <ericornelissen@gmail.com>
2018-02-08 18:02:34 +0100

    Test jekyll-fontello using MiniTest

--- test/test_jekyll-fontello.rb
+++ test/test_jekyll-fontello.rb
@@ -16,37 +16,67 @@ class JekyllFontelloTest < Minitest::Test
 
     # Initialize the default generator
     @generator.init(@config['fontello'])
-  end
 
-  def test_generate
-    #
-    generator = JekyllFontello::Generator.new
-
-    #
-    generator.generate(@site)
+    # Make sure none of the created files exists
+    File.delete(JekyllFontello::SESSION_FILE) if File.exist? JekyllFontello::SESSION_FILE
+    File.delete(JekyllFontello::ZIP_FILE) if File.exist? JekyllFontello::ZIP_FILE
   end
 
   def test_download_zip
     @generator.download_zip()
+    assert File.exist? JekyllFontello::ZIP_FILE
   end
 
-  def _test_extract_zip
+  def test_extract_zip
+    FileUtils.cp('test/fixtures/fontello.zip', JekyllFontello::ZIP_FILE)
     @generator.extract_zip(@site)
+
+    # Make sure the fonts are extracted
+    assert File.exist? 'test/_tmp/fonts/fontello.eot'
+    assert File.exist? 'test/_tmp/fonts/fontello.svg'
+    assert File.exist? 'test/_tmp/fonts/fontello.ttf'
+    assert File.exist? 'test/_tmp/fonts/fontello.woff'
+    assert File.exist? 'test/_tmp/fonts/fontello.woff2'
+
+    # Make sure the stylesheets are extracted
+    assert File.exist? 'test/_tmp/styles/animation.css'
+    assert File.exist? 'test/_tmp/styles/fontello-codes.css'
+    assert File.exist? 'test/_tmp/styles/fontello-embedded.css'
+    assert File.exist? 'test/_tmp/styles/fontello-ie7-codes.css'
+    assert File.exist? 'test/_tmp/styles/fontello-ie7.css'
+    assert File.exist? 'test/_tmp/styles/fontello.css'
   end
 
   def test_set_font_path
+    FileUtils.cp('test/fixtures/stylesheet.css', 'test/_tmp/styles/stylesheet.css')
     @generator.set_font_path()
+    assert_equal '@font-face { src: url(\'../foo/bar/fontello.eot?52513028\'); }',
+      File.read('test/_tmp/styles/stylesheet.css')
   end
 
-  def _test_clean_up
+  def test_clean_up
+    FileUtils.cp('test/fixtures/fontello.zip', JekyllFontello::ZIP_FILE)
     @generator.clean_up()
+    refute File.exist? JekyllFontello::ZIP_FILE
   end
 
   def test_session_key
-    refute_equal "", @generator.session_key
+    # Make sure there is a session key
+    session_key = @generator.session_key
+    refute_equal "", session_key
+    refute_nil session_key
+
+    # Make sure the same session is returned
+    assert_equal session_key, @generator.session_key
+
+    # Make sure a new session_key is requested if the previous one is lost
+    File.delete(JekyllFontello::SESSION_FILE)
+    refute_equal session_key, @generator.session_key
   end
 
   def test_relative_path_styles_to_fonts
-    @generator.relative_path_styles_to_fonts
+    # From fixtures/_config.yml, the relative path between
+    # 'test/_tmp/fonts' and 'test/_tmp/styles' is '../fonts'.
+    assert_equal Pathname.new("../fonts"), @generator.relative_path_styles_to_fonts
   end
 end
